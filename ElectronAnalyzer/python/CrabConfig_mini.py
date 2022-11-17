7af71ca7d7f055edc53cf288d8a7fc667fea5689
Normunds Strautnieks <79871354+pauris123@users.noreply.github.com>
2022-11-17 17:05:46 +0100

    Update CrabConfig_mini.py

--- ElectronAnalyzer/python/CrabConfig_mini.py
+++ ElectronAnalyzer/python/CrabConfig_mini.py
@@ -3,28 +3,28 @@ from CRABClient.UserUtilities import config
 
 config = config()
 
-#config.General.requestName = 'Bpark_MiniAOD_MC_reliso_UL' 
+#config.General.requestName = 'Bpark_MiniAOD_MC_reliso_UL' # Naming for CRAB and maps
 config.General.requestName = 'Bpark_MiniAOD_DATA_BPH4_runA_reliso_UL_v2' 
-config.General.workArea = 'crab_projects'
+config.General.workArea = 'crab_projects' # area where information about crab jobs will be saved. You should create this directory next to this file
 config.General.transferOutputs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'ConfFile_cfg.py'
+config.JobType.psetName = 'ConfFile_cfg.py' # CMS config code we wish to run on Crab jobs
 
 
-#config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18MiniAOD-106X_upgrade2018_realistic_v11_L1v1-v1/MINIAODSIM'
-#config.Data.inputDataset = '/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIIAutumn18MiniAOD-PUPoissonAve20_BParking_102X_upgrade2018_realistic_v15_ext1-v2/MINIAODSIM'
-config.Data.inputDataset = '/ParkingBPH4/Run2018A-UL2018_MiniAODv2-v1/MINIAOD' #'/ParkingBPH6/Run2018B-05May2019-v2/MINIAOD'                                      #'/ParkingBPH6/Run2018B-05May2019-v2/MINIAOD'
-#config.Data.inputDataset = '/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIISummer19UL18MiniAODv2-TrkExtra_106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM'
+config.Data.inputDataset = '/ParkingBPH4/Run2018A-UL2018_MiniAODv2-v1/MINIAOD' #'/ParkingBPH6/Run2018B-05May2019-v2/MINIAOD'  #DATA                                    #'/ParkingBPH6/Run2018B-05May2019-v2/MINIAOD'
+#config.Data.inputDataset = '/BuToKJpsi_Toee_Mufilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/RunIISummer19UL18MiniAODv2-TrkExtra_106X_upgrade2018_realistic_v16_L1v1-v1/MINIAODSIM' # MC
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'FileBased'
-#config.Data.splitting = 'Automatic'
+config.Data.splitting = 'FileBased' # IF we wish to run by files per job
+#config.Data.splitting = 'Automatic' # If we wish to optimize files per job (sometimes does not work)
 #config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 5 #
+config.Data.unitsPerJob = 5 #running 5 files per job
 #config.Data.lumiMask = 'https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
 #config.Data.runRange = '275776-275782'
 config.Data.publication = True
 #config.Data.outputDatasetTag = 'Bpark_MiniAOD_MC_reliso_UL'
-config.Data.outputDatasetTag = 'Bpark_MiniAOD_DATA_BPH4_runA_reliso_UL_v2'
+config.Data.outputDatasetTag = 'Bpark_MiniAOD_DATA_BPH4_runA_reliso_UL_v2' # Naming for CRAB and maps
 
-config.Site.storageSite = 'T3_CH_CERNBOX'
+config.Site.storageSite = 'T3_CH_CERNBOX' # writes the ouput inside your cernbox directoty under the name of 'inputDataset'
+
+# When this is done, submit CRAB jobs by using command "crab submit -c CrabConfig_mini.py"
